41ab9cd587ca2830615c641c8597fb832e7597a0
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-03-31 14:58:35 +0600

    added products

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -12,6 +12,7 @@ const Product = (props) => {
         <p>Manufacturer: {seller}</p>
         <p>Rating: {ratings}</p>
       </div>
+      <button className="btn-cart">Add to Cart</button>
     </div>
   );
 };
