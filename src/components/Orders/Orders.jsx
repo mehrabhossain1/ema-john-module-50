89c3964a0d82c578b25d16667728d166beb4d424
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-04-11 23:40:01 +0600

    loader added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,16 +3,16 @@ import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 
 const Orders = () => {
-  const products = useLoaderData();
-  console.log("7", products);
+  const cart = useLoaderData();
+  console.log("7", cart);
 
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h4>Orders Page: {products.length}</h4>
+        <h4>Orders Page: {cart.length}</h4>
       </div>
       <div className="cart-container">
-        <Cart cart={[]} />
+        <Cart cart={cart} />
       </div>
     </div>
   );
