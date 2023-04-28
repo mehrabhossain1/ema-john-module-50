93a1736e7080459ae844fb5c3b44d4a8d5344a7a
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-04-28 21:37:23 +0600

    signup page done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,12 +4,17 @@ import "./Login.css";
 const Login = () => {
   return (
     <div className="form-container">
-      <h2 className="form-title">login</h2>
+      <h2 className="form-title">Login</h2>
       <form>
         <div className="form-control">
-          <label htmlFor="">Email</label>
+          <label htmlFor="email">Email</label>
           <input type="email" name="email" id="" required />
         </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
       </form>
     </div>
   );
